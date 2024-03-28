5841fd43c375ce3e785143fd086e6889cacf20a4
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2024-03-28 23:02:22 +0900

    [Bronze V] Title: ??!, Time: 60 ms, Memory: 5376 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BaekJoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ID = Console.ReadLine();
+            Console.WriteLine(ID + "??!");
+        }
+    }
+}
