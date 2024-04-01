feee787877c7c26925d7b21e508fb1a98662d0f3
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2024-04-01 14:33:50 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 64 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] arges)
+        {
+            int y;
+            string year = Console.ReadLine();
+            y = Convert.ToInt32(year);
+            Console.WriteLine(y-543);
+        }
+    }
+}
