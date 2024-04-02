27bc5fee4c2f831ba8c22f4d1c23d912af5e613c
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2024-04-02 23:44:34 +0900

    [Bronze V] Title: 나머지, Time: 64 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace BaekJoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] num = Console.ReadLine().Split();
+            int A = int.Parse(num[0]);
+            int B = int.Parse(num[1]);
+            int C = int.Parse(num[2]);
+
+            Console.WriteLine((A + B) % C);
+            Console.WriteLine(((A % C) + (B % C)) % C);
+            Console.WriteLine((A * B) % C);
+            Console.WriteLine(((A % C) * (B % C)) % C);
+        }
+    }
+}
