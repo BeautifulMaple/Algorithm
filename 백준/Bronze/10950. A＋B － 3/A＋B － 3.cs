6587b3fadffda9e2115362d7f2e7452fca0b283b
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2024-09-04 14:51:45 +0900

    [Bronze V] Title: A+B - 3, Time: 52 ms, Memory: 5652 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int T = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < T; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+            Console.WriteLine(A + B);
+        }
+    }
+}
