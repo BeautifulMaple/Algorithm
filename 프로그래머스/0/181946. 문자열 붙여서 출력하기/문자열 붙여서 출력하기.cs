3ba045c4776432681dcbfa526a366dabaac21f7b
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2025-01-27 17:04:05 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 17.98 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String[] input = Console.ReadLine().Split(' ');
+
+        String str1 = input[0];
+        String str2 = input[1];
+        
+        Console.WriteLine(str1+str2);
+
+    }
+}
