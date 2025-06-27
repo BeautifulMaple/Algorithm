9167924cd1c7f4a504f9f5123b2a23fe6f3f9d11
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2025-06-27 21:35:56 +0900

    [level 1] Title: 약수의 합, Time: 0.24 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cs
@@ -0,0 +1,14 @@
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        // 약수 구하기
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+                answer += i;
+        }
+        return answer;
+    }
+}
